2acba9ae8ce25d195441c42c37d146e919d359f2
TheZoq2 <frans.skarman@gmail.com>
2019-05-14 21:09:46 +0200

    Lägg till fler nyckelord

--- sv.cpp
+++ sv.cpp
@@ -1,8 +1,7 @@
-#include "inkluderingar.hpp"
+#include "inkluderingar.hufvudpp"
 
 #define hel int
 #define hufvud main
-#define vidbränd char
 #define pekare *
 #define dubbelpekare pekare pekare
 #define referens_till &
@@ -11,24 +10,54 @@
 #define medans while
 #define placeras puts
 #define mall template
+#define typnamn typename
 #define sluta break
 #define om if
+#define annars else
+#define alternativt else if
 #define uppfostra throw
 #define offentligt public
 #define personligt private
-#define könssjukdom std
 #define snöre string
 #define bil auto
+#define evig const
+#define fackförbund union
+#define knapp switch
+#define fodral case
+#define rullskridsko inline
+#define default
+#define barnförbjudet explicit
+#define förinta delete
+#define gör do
+#define evig_gjutning const_cast
+#define icke_vid_kompileringstid_bestämd_gjutning dynamic_cast
+#define gratis free
+
+#define könssjukdom std
+
+#define herr_bool bool
+#define oskyltad unsigned
+#define dubbel double
+#define flyt float
+#define vidbränd char
+#define tomma_intet void
+#define inget NULL
+
+#define öppen_krullig_tandställning {
+#define stängd_krullig_tandställning }
+
+
+#define ökas_med_ett ++
 
 #define klass class
 
-klass Barn {
+klass Barn öppen_krullig_tandställning
 offentligt:
     Barn(könssjukdom::snöre meddelande) : meddelande(meddelande)
-        {}
+        öppen_krullig_tandställning stängd_krullig_tandställning
 personligt:
     könssjukdom::snöre meddelande;
-};
+stängd_krullig_tandställning;
 
 
 hel hufvud(hel arga, vidbränd dubbelpekare argv)
@@ -39,7 +68,7 @@ hel hufvud(hel arga, vidbränd dubbelpekare argv)
     medans (i < arga) {
         placeras(dereferera nuvarande);
         nuvarande++;
-        i++;
+        i ökas_med_ett;
         om (i == 3) {
             uppfostra Barn("För många argument");
         }
