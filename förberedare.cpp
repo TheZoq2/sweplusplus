73abcdaf60a48064b97bcc22bb520c85d74bf017
TheZoq2 <frans.skarman@gmail.com>
2019-05-14 22:00:20 +0200

    Nyckelord och annat som krävs förberedare

---
+++ förberedare.cpp
@@ -0,0 +1,22 @@
+#include "inkluderingar.hufvudpp"
+#include "nyckelord.hufvudpp"
+
+använder namnrymd könssjukdom;
+
+hel hufvud(hel arga, vidbränd dubbelpekare argv) öppen_krullig_tandställning
+    om (arga != 1) {
+        skrivutf("Användning:  <utdatafil>");
+        utgång(-1);
+    }
+
+    bil utdatafil = könssjukdom::snöre(dereferera argv);
+
+    könssjukdom::inbäck könssjukdomin = könssjukdom::swein;
+
+    könssjukdom::snöre indatafil;
+
+    medan (1) {
+        fålinje(könssjukdomin, indatafil)
+    } medan() != eofbit
+stängd_krullig_tandställning
+
