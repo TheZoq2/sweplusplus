0f34426e200ac33b5d7f775b1311240b948cc8f1
Axel Nordanskog <axeno840@student.liu.se>
2020-10-06 18:57:54 +0200

    Använd korrekt sweplusplus i föreberedare.cpp
    Tidigare kompilerade inte koden men än värre: Den använde också de
    kätterska C++-symbolerna { och }.

--- förberedare.cpp
+++ förberedare.cpp
@@ -4,19 +4,18 @@
 använder namnrymd könssjukdom;
 
 hel hufvud(hel arga, vidbränd dubbelpekare argv) öppen_krullig_tandställning
-    om (arga != 1) {
+    om (arga != 1) öppen_krullig_tandställning
         skrivutf("Användning:  <utdatafil>");
         utgång(-1);
-    }
+    stängd_krullig_tandställning
 
     bil utdatafil = könssjukdom::snöre(dereferera argv);
 
-    könssjukdom::inbäck könssjukdomin = könssjukdom::swein;
-
     könssjukdom::snöre indatafil;
 
-    medan (1) {
-        fålinje(könssjukdomin, indatafil)
-    } medan() != eofbit
+    gör öppen_krullig_tandställning
+        fålinje(könssjukdom::swein, indatafil);
+    stängd_krullig_tandställning
+    medans (!könssjukdom::swein.eof());
 stängd_krullig_tandställning
 
